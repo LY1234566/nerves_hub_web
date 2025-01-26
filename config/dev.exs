789626c0f5cfce418e236eb1295e5899430e95d6
LY1234566 <3611960790@qq.com>
2025-01-26 20:40:10 +0800

    22

--- config/dev.exs
+++ config/dev.exs
@@ -8,7 +8,7 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 
 
 ##
-# NervesHub Web
+# NervesHub 
 #
 config :nerves_hub, NervesHubWeb.Endpoint,
   url: [
